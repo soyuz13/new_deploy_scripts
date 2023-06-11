fe6b93dea1da1ba7d6ced3622e29474a3a50f2e9
root <root@59595.cloudx.site>
2023-06-11 22:47:47 +0000

    newfas

---
+++ start_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade
+apt-get install mc
+apt-get install language-pack-ru
+apt-get install python3.9
+#apt-get install git
+apt-get install python3-pip
+apt-get install python3.9-venv
