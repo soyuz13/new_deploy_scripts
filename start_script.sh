dc10ba5d4c65b72bf96bc97045b5f144502677c8
root <root@59595.cloudx.site>
2023-06-13 21:34:51 +0000

    qwer

--- start_script.sh
+++ start_script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  обновление системы
 apt-get -y update && apt-get -y upgrade
-apt-get -y install mc
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  установка языка и локали
 apt-get -y install language-pack-ru
+update-locale LANG=C.UTF-8
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  установка mc
+apt-get -y install mc
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  установка python3.9
 apt-get -y install python3.9
 #apt-get install git
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  установка pip
 apt-get -y install python3-pip
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  установка venv
 apt-get -y install python3.9-venv
