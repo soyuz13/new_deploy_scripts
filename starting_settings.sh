e73698dc456f008b96b5bc644d38a4968ca219e5
soyuz13 <52010985+soyuz13@users.noreply.github.com>
2023-11-12 13:57:53 +1000

    Update and rename start_settings.sh to starting_settings.sh

---
+++ starting_settings.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  update and upgrade system
+echo
+apt-get -y update && apt-get -y upgrade
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  install language pack and C.UTF-8 locale 
+echo
+apt-get -y install language-pack-ru
+update-locale LANG=C.UTF-8
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  install mc
+echo
+apt-get -y install mc
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  install python3.9
+echo
+apt-get -y install python3.9
+#apt-get install git
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  install pip
+echo
+apt-get -y install python3-pip
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  install venv
+echo
+apt-get -y install python3.9-venv
+echo
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Logout and login for apply RU-language settings
+echo
